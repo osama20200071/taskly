8a7f4ad73035a06691aef1067924c9b0e71e528f
osamakaram370z <osamakaram118@gmail.com>
2024-09-29 17:57:10 +0300

    adding the history and finish the course

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,20 +1,72 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList, Pressable } from "react-native";
+import { getFromStorage } from "../../storage";
+import { countDownStorageKey, PersistedCountDownState } from ".";
+import { format } from "date-fns";
+import { theme } from "../../theme";
 
+const fullDateFormat = `LLL d yyyy, h:mm aaa`;
 export default function HistoryScreen() {
+  const [countDownState, setCountDownState] = useState<PersistedCountDownState>(
+    { completedAtTimestamps: [], currentNotificationId: undefined },
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = (await getFromStorage(
+        countDownStorageKey,
+      )) as PersistedCountDownState;
+      setCountDownState(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countDownState?.completedAtTimestamps}
+      renderItem={({ item }) => (
+        <Pressable>
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>
+              {format(item, fullDateFormat)}
+            </Text>
+          </View>
+        </Pressable>
+      )}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>Your shopping list is empty</Text>
+        </View>
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
     alignItems: "center",
+    marginVertical: 18,
   },
-  text: {
-    fontSize: 24,
+  listItem: {
+    marginHorizontal: 8,
+    marginBottom: 8,
+    alignItems: "center",
+    backgroundColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+  },
+  listItemText: {
+    fontSize: 16,
   },
 });
--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,22 +1,87 @@
-import { Text, View, StyleSheet, Alert } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  useWindowDimensions,
+} from "react-native";
 import Button from "../../components/Button";
 import { registerForPushNotificationsAsync } from "../../registerForPushNotificationsAsync";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
+import { useEffect, useRef, useState } from "react";
+import { Duration, intervalToDuration, isBefore } from "date-fns";
+import { TimeSegment } from "../../components/TimeSegment";
+import { theme } from "../../theme";
+import { getFromStorage, saveToStorage } from "../../storage";
+import ConfettiItem from "react-native-confetti-cannon";
+
+// after 10 seconds form the app starts
+const frequency = 24 * 60 * 60 * 1000;
+export const countDownStorageKey = "taskly-counter";
+
+export type PersistedCountDownState = {
+  currentNotificationId: string | undefined;
+  completedAtTimestamps: number[];
+};
+
+type CountDownStatus = {
+  isOver: boolean;
+  remainingTime: Duration;
+};
 
 export default function CounterScreen() {
-  const scheduleNotification = async () => {
-    const result = await registerForPushNotificationsAsync();
-    console.log(result);
+  const confettiRef = useRef<any>();
+  const { width } = useWindowDimensions();
+  const [status, setStatus] = useState<CountDownStatus>();
+  const [countDownState, setCountDownState] =
+    useState<PersistedCountDownState>();
 
+  const lastCompletedTimestamp = countDownState?.completedAtTimestamps[0];
+
+  useEffect(() => {
+    const init = async () => {
+      const data = await getFromStorage(countDownStorageKey);
+      setCountDownState(data);
+    };
+    init();
+  }, []);
+
+  // so whenever we complete a task we add the timestamp to the completedAtTimestamps
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // if that was completed before we add the frequency else start from now
+      const timestamp = lastCompletedTimestamp
+        ? lastCompletedTimestamp + frequency
+        : Date.now();
+
+      const isOver = isBefore(timestamp, Date.now());
+      const remainingTime = intervalToDuration(
+        isOver
+          ? { start: timestamp, end: Date.now() }
+          : { start: Date.now(), end: timestamp },
+      );
+      setStatus({ isOver, remainingTime });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [lastCompletedTimestamp]);
+
+  const scheduleNotification = async () => {
+    let notificationId;
+    confettiRef.current.start(); // show the confetti when the task is completed and scheduling a new notification
+    const result = await registerForPushNotificationsAsync();
+    // console.log(result);
     if (result === "granted") {
-      await Notifications.scheduleNotificationAsync({
+      notificationId = await Notifications.scheduleNotificationAsync({
         content: {
           title: "Hello",
-          body: "I'm a scheduled notification",
+          body: "Time to wash the car!",
         },
         trigger: {
-          seconds: 5,
+          seconds: frequency / 1000,
         },
       });
     } else {
@@ -27,12 +92,57 @@ export default function CounterScreen() {
         );
       }
     }
+
+    // if there is a scheduled notification we need to cancel it
+    if (countDownState?.currentNotificationId) {
+      await Notifications.cancelScheduledNotificationAsync(
+        countDownState?.currentNotificationId,
+      );
+    }
+    // if there is no scheduled notification before
+    // then we create the new countdown state with this new scheduled notification
+    const newCountDownState: PersistedCountDownState = {
+      currentNotificationId: notificationId,
+      completedAtTimestamps: countDownState?.completedAtTimestamps
+        ? [Date.now(), ...countDownState.completedAtTimestamps]
+        : [Date.now()],
+    };
+    setCountDownState(newCountDownState);
+    await saveToStorage(countDownStorageKey, newCountDownState);
   };
 
   return (
-    <View style={styles.container}>
-      <Button onClick={scheduleNotification}>Rqs for Permission</Button>
-      <Text style={styles.text}>Counter</Text>
+    <View
+      style={[
+        styles.container,
+        status?.isOver ? styles.lateContainer : undefined,
+      ]}
+    >
+      {status?.isOver ? (
+        <Text style={styles.text}>Car Wash overdue by</Text>
+      ) : (
+        <Text style={styles.text}>Next Car Wash due in ...</Text>
+      )}
+      <View style={styles.timerContainer}>
+        <TimeSegment unit="Days" number={status?.remainingTime.days ?? 0} />
+        <TimeSegment unit="Hours" number={status?.remainingTime.hours ?? 0} />
+        <TimeSegment
+          unit="Minutes"
+          number={status?.remainingTime.minutes ?? 0}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status?.remainingTime.seconds ?? 0}
+        />
+      </View>
+      <Button onClick={scheduleNotification}>I've washed the Car!</Button>
+      <ConfettiItem
+        count={50}
+        origin={{ x: width / 2, y: -20 }}
+        fadeOut
+        ref={confettiRef}
+        autoStart={false}
+      />
     </View>
   );
 }
@@ -44,8 +154,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
+
+  lateContainer: {
+    backgroundColor: theme.colorRed,
+  },
   text: {
     fontSize: 24,
     textAlign: "center",
+  },
+
+  timerContainer: {
+    flexDirection: "row",
+    marginVertical: 24,
   },
 });
